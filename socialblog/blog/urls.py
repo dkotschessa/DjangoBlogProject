1788cd1ea2a018a9e7fb2dfbe3667d2ee08e9c3c
dkotschessa <itzdavey@gmail.com>
2022-08-31 11:16:35 -0400

    added django extensions

--- socialblog/blog/urls.py
+++ socialblog/blog/urls.py
@@ -5,16 +5,3 @@ from django.views.generic import TemplateView, ListView
 from django.utils import timezone
 
 urlpatterns = [path("about/", views.AboutView.as_view(), name="about")]
-
-
-class AboutView(TemplateView):
-    template_name = "about.html"
-
-
-class PostListView(ListView):
-    model = Post
-
-    def get_queryset(self):
-        return Post.objects.filter(
-            published_date__lte=timezone.now.order_by("-published_date")
-        )
